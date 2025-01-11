f4c2147627ceb388b84e24d05a0ac2b14feb4ce1
aakashnnt123 <aakash5427896@gmail.com>
2025-01-11 18:10:49 +0530

    cmmit

--- Src/Routes/UserRoutes.js
+++ Src/Routes/UserRoutes.js
@@ -2,7 +2,10 @@ const express = require('express');
 const { UserAuth } = require('../middlewares/Userauth');
 const UserRouter = express.Router();
 const connectionRequestModel = require('../Models/connectionRequest');
+const UserModel = require('../Models/user');
 
+
+const USER_SAFE_DATA = "firstName lastName photoUrl age gender about Skills"
 // Get all the pending connection request for the loggedIn user..
 UserRouter.get('/user/requests/received'  , UserAuth , async (req , res)=>{
   try{
@@ -34,7 +37,7 @@ UserRouter.get('/user/connections'  , UserAuth , async (req , res)=>{
             Status : "accepted",}
         ]
 
-      }).populate("fromUserId" ,["firstName" , "lastName"]).populate("toUserId");
+      }).populate("fromUserId" , USER_SAFE_DATA).populate("toUserId");
       
       const data = connectionRequest.map((row)=>{
         if(row.fromUserId._id.toString() === loggedInuser._id.toString())
@@ -52,6 +55,43 @@ UserRouter.get('/user/connections'  , UserAuth , async (req , res)=>{
   }
 });
 
+
+// User Feed API...
+UserRouter.get('/feed' , UserAuth , async(req , res)=>{
+  try{
+    const loggedInuser = req.user;
+
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
+    const skip = (page-1)*limit;
+
+    const connectionRequest =  await connectionRequestModel.find({
+      $or:[
+        {fromUserId : loggedInuser._id},
+        {toUserId : loggedInuser._id},
+      ],
+    }).select("fromUserId  toUserId")
+
+    const HideUserFromFeed = new Set();
+     connectionRequest.forEach((req) => {
+      HideUserFromFeed.add(req.fromUserId.toString
+        ());
+        HideUserFromFeed.add(req.toUserId.toString());
+     });
+    
+     
+     const ShowUsers = await UserModel.find({
+      $and: [
+        {_id : {$nin: Array.from(HideUserFromFeed)}},
+        {_id:{$ne:loggedInuser._id}}
+      ],
+     }).select(USER_SAFE_DATA).skip(skip).limit(limit)
+     res.send(ShowUsers)
+  }
+  catch (err) {
+    res.status(400).send('ERROR: ' + err.message);
+  }
+})
 
 
 module.exports = UserRouter;
